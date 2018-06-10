a44deb7deb3ecaf11360fad7fd15b67a45601d37
Daniel <daniel.skalski@gmail.com>
2018-06-10 20:57:48 +0200

    added unit tests

---
+++ Tests/WebAppCdTest2.Tests/AboutModelTests.cs
@@ -0,0 +1,25 @@
+﻿using WebAppCdTest2.Pages;
+using Xunit;
+
+namespace WebAppCdTest2.Tests
+{
+    public class AboutModelTests
+    {
+        private readonly AboutModel _model; 
+
+        public AboutModelTests()
+        {
+            _model = new AboutModel();
+        }
+
+        [Fact]
+        public void AboutModel_OnGet_HasCorrectMessage()
+        {
+            // act
+            _model.OnGet();
+
+            // assert
+            Assert.Equal("Your application description page.", _model.Message);
+        }
+    }
+}
---
+++ Tests/WebAppCdTest2.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace WebAppCdTest2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestToTestTesting()
+        {
+            var a = true;
+
+
+            Assert.True(a);
+        }
+    }
+}
